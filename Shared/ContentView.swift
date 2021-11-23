e1fec0019a5c4f4c1d6594e1da66aaa6589e705a
Akın Özgen <akinozgen17@outlook.com>
2021-11-23 19:04:51 +0300

    Weather status icon and degrees

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -20,6 +20,31 @@ struct ContentView: View {
             
             VStack { // Main Content
                 
+                // City Name
+                Text("Çanakkale, TR")
+                    .font(
+                        .system(size: 32, weight: .medium, design: .rounded)
+                    )
+                    .foregroundColor(.white)
+                    .padding()
+                
+                // Weather Status Icon and weather in degrees
+                VStack(spacing: 8) {
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 180, height: 180, alignment: .center)
+                    
+                    
+                    Text("18°")
+                        .font(.system(size: 70, weight: .semibold, design: .rounded))
+                        .foregroundColor(.white)
+                    
+                }
+                // Weather status ends
+                
+                Spacer()
             } // Main Content Ends
         }
     }
