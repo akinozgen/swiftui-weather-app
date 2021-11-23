135827e37c8282b46ac827e8699e1293c9905d61
Akın Özgen <akinozgen17@outlook.com>
2021-11-23 18:21:19 +0300

    Initial Commit

---
+++ Shared/Just_Another_Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Just_Another_Weather_AppApp.swift
+//  Shared
+//
+//  Created by Akın Özgen on 23.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct Just_Another_Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
